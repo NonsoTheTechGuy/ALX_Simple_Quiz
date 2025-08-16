e87c8ddc16950e36ead666439c8539c2b3083862
Kalu Jonah Chinonso <kaluchinonso18@gmail.com>
2025-08-16 22:13:54 +0100

    implementing a JavaScript functionality for a quiz application

--- quiz.js
+++ quiz.js
@@ -10,7 +10,7 @@ function checkAnswer() {
         return;
     }
     // if (userAnswer === correctAnswer) {}
-    if(userAnswer.value === correctAnswer) {
+    if (userAnswer.value === correctAnswer) {
         feedback.textContent = "correct! well done";
         // feedback.style.color = "green";
     } else {
