a179c7f2f5ac5bbde6cad46c99d7d0268165b571
Kalu Jonah Chinonso <kaluchinonso18@gmail.com>
2025-08-16 21:43:51 +0100

    implementing a JavaScript functionality for a quiz application

--- quiz.js
+++ quiz.js
@@ -1,4 +1,23 @@
-// function checkAnswer(){
-//     let correctAnswer = "4";
-//     document.querySelector('')
-// }
+function checkAnswer(){
+    const correctAnswer = "4";
+    // Get the selected answer (radio button)
+    const userAnswer = document.querySelector('input[name="answer"]:checked');
+    const feedback = document.getElementById('feedback');
+
+    if(!userAnswer){
+        feedback.textContent = "Please select an answer";
+        feedback.style.color = "orange"
+        return
+    }
+
+    if(userAnswer === correctAnswer){
+        feedback.textContent = "correct! well done";
+        feedback.style.color = "green";
+    } else {
+        feedback.textContent = "That's Incorrect, Try again!.";
+        feedback.style.color = "red";
+    }
+}
+
+// Add event listener to the "Submit Answer" button
+document.getElementById('submit-answer').addEventListener('click', checkAnswer);
